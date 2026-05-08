de70f43ef790cc1fa0337a96b21308cd2e54b2b3
sulaimankhanCs <sulaimankhank641@gmail.com>
2026-05-08 12:28:41 +0500

    connected mongodb database

---
+++ youtube-app-backend/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import app from './app.js';
+
+dotenv.config({
+    path: './env'
+});
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+});
